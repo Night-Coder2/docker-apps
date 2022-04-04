82592899126379ea57cc7a498471dc240ff730d7
sudhir28-creator <84770654+sudhir28-creator@users.noreply.github.com>
2022-04-04 10:54:34 +0530

    Update docker-stup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -4,4 +4,3 @@ sudo apt-get install libffi-dev libssl-dev
 sudo apt install python3-dev
 sudo apt-get install -y python3 python3-pip
 sudo pip3 install docker-compose
-sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
