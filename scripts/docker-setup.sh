68a5c1d8992349b7c63a7e2fc7a0caa70fa67cd1
sudhir28-creator <84770654+sudhir28-creator@users.noreply.github.com>
2022-04-10 18:22:25 +0530

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sSL https://get.docker.com | sh
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install libffi-dev libssl-dev -y
+sudo apt install python3-dev -y
+sudo apt-get install -y python3 python3-pip -y
 sudo pip3 install docker-compose
