8de43d217ad681975f1811538232f2a34ded3420
sudhir28-creator <84770654+sudhir28-creator@users.noreply.github.com>
2022-04-11 21:04:55 +0530

    Update docker-compose.yml

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -4,4 +4,7 @@ sudo apt-get install libffi-dev libssl-dev -y
 sudo apt install python3-dev -y
 sudo apt-get install -y python3 python3-pip -y
 sudo pip3 install docker-compose
-docker network create -d macvlan --subnet 192.168.0.0/24 --gateway 192.168.0.1 --ip-range 192.168.0.253/32 -o parent=eth0 macvlan
+myname=$(whoami)
+sudo usermod -aG docker $myname
+sudo docker network create -d macvlan --subnet 192.168.0.0/24 --gateway 192.168.0.1 --ip-range 192.168.0.253/32 -o parent=eth0 macvlan
+sudo reboot
