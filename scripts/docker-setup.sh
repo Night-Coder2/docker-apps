ac153dc8e651e1acf007e75351597168c8c471fb
Phoenixlegen <sudhircks@gmail.com>
2022-04-17 13:10:39 +0530

    do

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -4,7 +4,7 @@ sudo apt upgrade -y
 curl -sSL https://get.docker.com | sh
 sudo apt-get install libffi-dev libssl-dev -y
 sudo apt install python3-dev -y
-sudo apt-get install -y python3 python3-pip -y
+sudo apt-get install python3 python3-pip -y
 sudo pip3 install docker-compose
 myname=$(whoami)
 sudo usermod -aG docker $myname
