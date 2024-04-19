b550f20f3880668e92ac73e6a17153c5c2aca537
Night-Coder2 <84770654+Night-Coder2@users.noreply.github.com>
2024-04-19 14:31:54 +0530

    final commit

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -12,3 +12,11 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+myname=$(whoami)
+sudo usermod -aG docker $myname
+sudo docker network create -d macvlan --subnet 192.168.0.0/24 --gateway 192.168.0.1 --ip-range 192.168.0.253/32 -o parent=eth0 macvlan
+
+sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+mkdir ~/docker
+sudo reboot
